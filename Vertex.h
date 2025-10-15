8ada5a609bded58c8dd0560773867c00a90930d2
liviacardaccia02 <liviacardaccia99@gmail>
2025-10-15 20:14:51 +0200

    order of coordinates corrected

--- Vertex.h
+++ Vertex.h
@@ -5,17 +5,17 @@ class Vertex
 {
 private:
     int id;
-    float latitude, longitude;
+    double longitude, latitude;
 
 public:
     /* Constructor */
-    Vertex(int id, float latitude, float longitude) : id(id), latitude(latitude), longitude(longitude) {}
+    Vertex(int id, double longitude, double latitude) : id(id), longitude(longitude), latitude(latitude) {}
     ~Vertex() = default;
 
     /* Getters */
     int getId() const { return id; }
-    float getLatitude() const { return latitude; }
-    float getLongitude() const { return longitude; }
+    double getLongitude() const { return longitude; }
+    double getLatitude() const { return latitude; }
 };
 
 #endif
