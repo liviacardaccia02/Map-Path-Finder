082e59e7ff428adfc7fb98d35a0a5bf5b1025e5f
liviacardaccia02 <liviacardaccia99@gmail>
2025-10-22 21:56:36 +0200

    implementation of A star working properly - to be tested further

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,9 +1,11 @@
 #include "algorithms.h"
+#include "utils.h"
 #include <iomanip>
 #include <algorithm>
 #include <queue>
 #include <set>
 #include <limits>
+#include <chrono>
 
 void algorithms::bfs(const Graph &graph, uint32_t startVertexId, uint32_t endVertexId)
 {
@@ -24,6 +26,8 @@ void algorithms::bfs(const Graph &graph, uint32_t startVertexId, uint32_t endVer
     }
     else
     {
+        auto start = std::chrono::steady_clock::now();
+
         std::queue<uint32_t> queue;                    // Queue of vertex IDs to visit
         std::unordered_set<uint32_t> visited;          // Set of visited vertex IDs
         std::unordered_map<uint32_t, uint32_t> parent; // To reconstruct the path
@@ -40,7 +44,7 @@ void algorithms::bfs(const Graph &graph, uint32_t startVertexId, uint32_t endVer
 
         while (!queue.empty())
         {
-            int current = queue.front();
+            uint32_t current = queue.front();
             queue.pop();
             visitedCount++;
 
@@ -61,9 +65,19 @@ void algorithms::bfs(const Graph &graph, uint32_t startVertexId, uint32_t endVer
                 std::cout << "Total vertices on path from start to end = " << path.size() << std::endl;
                 for (size_t i = 0; i < path.size(); ++i)
                 {
-                    std::cout << "Vertex[" << std::setw(4) << (i + 1) << "] = " << std::setw(8) << path[i].first
+                    std::cout << "Vertex[" << std::setw(4) << (i + 1) << "] : id = " << std::setw(8) << path[i].first
                               << ", length = " << std::fixed << std::setprecision(2) << path[i].second << std::endl;
                 }
+
+                auto end = std::chrono::steady_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+                std::string timeStr = std::to_string(duration);
+                if (timeStr.length() > 3)
+                {
+                    timeStr.insert(timeStr.length() - 3, ",");
+                }
+                std::cout << "INFO: path calculated in " << timeStr << "us" << std::endl;
+
                 return;
             }
 
@@ -108,6 +122,8 @@ void algorithms::dijkstra(const Graph &graph, uint32_t startVertexId, uint32_t e
     }
     else
     {
+        auto start = std::chrono::steady_clock::now();
+
         int visitedCount = 0;
         std::unordered_set<uint32_t> visited;            // Set of visited vertex IDs
         std::unordered_map<uint32_t, uint32_t> previous; // To reconstruct the path
@@ -116,8 +132,7 @@ void algorithms::dijkstra(const Graph &graph, uint32_t startVertexId, uint32_t e
 
         for (const auto &pair : graph.getVertices())
         {
-            uint32_t vertexId = pair.first;
-            distance[vertexId] = std::numeric_limits<double>::infinity(); // All vertices initially at infinite distance
+            distance[pair.first] = std::numeric_limits<double>::infinity(); // All vertices initially at infinite distance
         }
 
         distance[startVertexId] = 0.0;                                  // Distance to start vertex is 0
@@ -195,16 +210,136 @@ void algorithms::dijkstra(const Graph &graph, uint32_t startVertexId, uint32_t e
             std::cout << "Total vertices on path from start to end = " << path.size() << std::endl;
             for (size_t i = 0; i < path.size(); ++i)
             {
-                std::cout << "Vertex[" << std::setw(4) << (i + 1) << "] = " << std::setw(8) << path[i].first
+                std::cout << "Vertex[" << std::setw(4) << (i + 1) << "] : id = " << std::setw(8) << path[i].first
                           << ", length = " << std::fixed << std::setprecision(2) << path[i].second << std::endl;
             }
+
+            auto end = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+            std::string timeStr = std::to_string(duration);
+            if (timeStr.length() > 3)
+            {
+                timeStr.insert(timeStr.length() - 3, ",");
+            }
+            std::cout << "INFO: path calculated in " << timeStr << "us" << std::endl;
+
             return;
         }
     }
 }
 
+double heuristic(const Vertex &current, const Vertex &goal)
+{
+    return utils::haversineDistance(current, goal);
+}
+
 void algorithms::aStar(const Graph &graph, uint32_t startVertexId, uint32_t goalVertexId)
 {
+    if (startVertexId == goalVertexId)
+    {
+        std::cout << "Start and goal vertices are the same." << std::endl;
+        return;
+    }
+    else if (graph.getNeighbors(startVertexId).empty() || graph.getNeighbors(goalVertexId).empty())
+    {
+        std::cout << "Start or goal vertex has no neighbors in the graph." << std::endl;
+        return;
+    }
+    else if (graph.getVertices().find(startVertexId) == graph.getVertices().end() || graph.getVertices().find(goalVertexId) == graph.getVertices().end())
+    {
+        std::cout << "Start or goal vertex not found in the graph." << std::endl;
+        return;
+    }
+    else
+    {
+        auto start = std::chrono::steady_clock::now();
 
-    return;
+        int visitedCount = 0;
+        std::unordered_set<uint32_t> visited;            // Set of visited vertex IDs
+        std::unordered_map<uint32_t, uint32_t> previous; // To reconstruct the path
+        std::unordered_map<uint32_t, double> distance;   // To track cumulative distances - g(n)
+        std::set<std::pair<double, uint32_t>> pq;        // Min-heap priority queue {f(n), vertexId}
+
+        const Vertex &goalVertex = graph.getVertex(goalVertexId);
+
+        for (const auto &pair : graph.getVertices())
+        {
+            uint32_t vertexId = pair.first;
+            distance[vertexId] = std::numeric_limits<double>::infinity();
+        }
+
+        distance[startVertexId] = 0.0;
+        previous[startVertexId] = std::numeric_limits<uint32_t>::max();
+        pq.insert({heuristic(graph.getVertex(startVertexId), goalVertex), startVertexId});
+
+        while (!pq.empty())
+        {
+            uint32_t currentVertexId = pq.begin()->second;
+            pq.erase(pq.begin());
+
+            if (visited.find(currentVertexId) != visited.end())
+                continue;
+            visited.insert(currentVertexId);
+            visitedCount++;
+
+            if (currentVertexId == goalVertexId)
+                break;
+
+            const std::vector<Edge> &neighbors = graph.getNeighbors(currentVertexId);
+            for (const Edge &edge : neighbors)
+            {
+                uint32_t neighbor = edge.getEndId();
+                double weight = edge.getWeight();
+                double g = distance[currentVertexId] + weight;
+
+                if (g < distance[neighbor])
+                {
+                    if (distance[neighbor] != std::numeric_limits<double>::infinity())
+                        pq.erase({distance[neighbor] + heuristic(graph.getVertex(neighbor), goalVertex), neighbor});
+                    distance[neighbor] = g;
+                    previous[neighbor] = currentVertexId;
+                    double f = g + heuristic(graph.getVertex(neighbor), goalVertex);
+                    pq.insert({f, neighbor});
+                }
+            }
+        }
+
+        double shortestDistance = distance[goalVertexId];
+        std::vector<uint32_t> path;
+        if (shortestDistance == std::numeric_limits<double>::infinity())
+        {
+            std::cout << "No path found from vertex " << startVertexId << " to vertex " << goalVertexId << ".\n"
+                      << std::endl;
+            return;
+        }
+        else
+        {
+            uint32_t u = goalVertexId;
+            while (u != std::numeric_limits<uint32_t>::max())
+            {
+                path.push_back(u);
+                u = previous[u];
+            }
+            std::reverse(path.begin(), path.end());
+
+            std::cout << "Total visited vertex = " << visitedCount << std::endl;
+            std::cout << "Total vertices on path from start to end = " << path.size() << std::endl;
+            for (size_t i = 0; i < path.size(); ++i)
+            {
+                std::cout << "Vertex[" << std::setw(4) << (i + 1) << "] : id = " << std::setw(8) << path[i]
+                          << ", length = " << std::fixed << std::setprecision(2) << distance[path[i]] << std::endl;
+            }
+
+            auto end = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+            std::string timeStr = std::to_string(duration);
+            if (timeStr.length() > 3)
+            {
+                timeStr.insert(timeStr.length() - 3, ",");
+            }
+            std::cout << "INFO: path calculated in " << timeStr << "us" << std::endl;
+
+            return;
+        }
+    }
 }
